614d956657b0a71a5290d204da1466801fa1622c
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-23 20:52:47 +0200

    install tableView

--- MVVMDemo_EditProfile/View/ProfileViewController.swift
+++ MVVMDemo_EditProfile/View/ProfileViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    //MARK: - Properties
+    private lazy var tableView: UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "EditProfile")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        configureNavigationBar()
+        
+        
     }
+}
 
+//MARK: - SETUP UI
+extension ProfileViewController {
+    
+    private func setupUI() {
+        view.addSubview(tableView)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5.0).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 5.0).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -5.0).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -5.0).isActive = true
+        
+        
+        
+    }
+    
+    private func configureNavigationBar() {
+        title = "Edit Profile"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(addButtonAction))
+    }
+    
+    @objc private func addButtonAction() {
+        let alert = UIAlertController(title: "Edit Profile", message: "This is mvvm demo.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default) { _ in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+}
 
+//MARK: - TableView
+extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
 
